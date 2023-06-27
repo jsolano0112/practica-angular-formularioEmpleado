cd3d677349784b3ab54f00569c96bee47cea2ab2
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-06-26 19:52:35 -0500

    Adding property and @input

--- src/app/employee-son/employee-son.component.ts
+++ src/app/employee-son/employee-son.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Employee } from '../employee.models';
 
 @Component({
   selector: 'app-employee-son',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class EmployeeSonComponent {
 
+  @Input() employeeList: Employee;
+
+  @Input() idx: number;
 }
