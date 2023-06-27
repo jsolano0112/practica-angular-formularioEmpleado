1e890e2c9fadefe88280560dbda6b27c230d31ae
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-06-27 15:01:35 -0500

    New component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { EmployeeSonComponent } from './employee-son/employee-son.component';
+import { CharacteristicsComponent } from './characteristics/characteristics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeSonComponent
+    EmployeeSonComponent,
+    CharacteristicsComponent
   ],
   imports: [
     BrowserModule,
