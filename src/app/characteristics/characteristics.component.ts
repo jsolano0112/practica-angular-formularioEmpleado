e9bd737763545f886d4148db3fc64aadf8adea67
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-06-28 09:59:56 -0500

    Adding output

--- src/app/characteristics/characteristics.component.ts
+++ src/app/characteristics/characteristics.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-characteristics',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./characteristics.component.css']
 })
 export class CharacteristicsComponent {
+  @Output() chEmployee = new EventEmitter<string>();
 
+  addCh(value:string){
+    this.chEmployee.emit(value);
+  }
 }
