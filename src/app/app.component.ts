721e27e3ec06d9253bc396189dd4121fae3716b0
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-06-28 17:12:02 -0500

    Updating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,17 @@ import { Employee } from './employee.models';
 export class AppComponent {
   title = 'Employee List';
 
- 
+  employees: Employee[] = [
+    new Employee("Juana","Solano","Engineer",1000000000),
+    new Employee("Patricia","Perez","Lawyer",7000),
+    new Employee("Jorge","Sanchez","Doctor",22222),
+  ];
+
+  AddingEmployee(){
+    let myEmployee = new Employee(this.cName, this.cLastname, this.cPosition, this.cSalary)
+    this.employees.push(myEmployee)
+  }
+
   cName : string = "";
   cLastname : string = "";
   cPosition : string = ""; 
